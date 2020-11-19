d05b1b2fb07451873e09e5b7c0b5386e298c2e54
camden9 <camdenvaughan@knights.ucf.edu>
2020-11-18 23:05:25 -0500

    Finished Tutorial
    Finished Jason Weimann Tutorial https://www.youtube.com/watch?v=Lu76c85LhGY

--- Birds/Assets/Scripts/Monster.cs
+++ Birds/Assets/Scripts/Monster.cs
@@ -15,7 +15,7 @@ public class Monster : MonoBehaviour
     {
         if (ShouldDieFromCollision(collision))
         {
-            Die();
+            StartCoroutine(Die());
         }
     }
 
@@ -33,11 +33,12 @@ public class Monster : MonoBehaviour
         else return false;
     }
 
-    void Die()
+    IEnumerator Die()
     {
         _hasDied = true;
         GetComponent<SpriteRenderer>().sprite = _deadSprite;
         _particleSystem.Play();
-        //gameObject.SetActive(false);
+        yield return new WaitForSeconds(1);
+        gameObject.SetActive(false);
     }
 }
