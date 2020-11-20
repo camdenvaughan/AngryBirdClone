331693fb5700374a924fee07d52b5d16315059c8
camden9 <camdenvaughan@knights.ucf.edu>
2020-11-19 23:43:58 -0500

    Fixed Bug in MenuBird.cs & Updated Menu Scene
    Fixed bug that caused multiple instances of the MenuBird.cs Co-Routines to execute, causing multiple launches. Also added cinemachine camera to menu scene

--- Birds/Assets/Scripts/MenuBird.cs
+++ Birds/Assets/Scripts/MenuBird.cs
@@ -5,23 +5,20 @@ using UnityEngine;
 public class MenuBird : MonoBehaviour
 {
 
-    [SerializeField] float _launchForce = 500f;
-    [SerializeField] float _maxDragDistance = 5f;
+    [SerializeField] float _launchForce = 1000f;
+    [SerializeField] float _maxDragDistance = 3f;
+    [SerializeField] float _maxYHeight = 0.9f;
+    [SerializeField] float _minYHeight = -1.5f;
+    [SerializeField] float _dragSpeed = 10f;
 
-    Vector2 _startPosition;
     Rigidbody2D _rigidbody2D;
     Animator _animator;
 
-    [SerializeField] float _maxYHeight = 0.9f;
-    [SerializeField] float _minYHeight = -1.5f;
-
-    [SerializeField] float _dragSpeed;
-    private bool _canDrag;
-
-
-    Vector2 _desiredPosition;
+    Vector2 _startPosition;
     Vector2 _randomDragPosition;
     Vector2 _direction;
+
+    bool _canDrag;
 
     void Awake()
     {
@@ -32,59 +29,67 @@ public class MenuBird : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Cache _startPosition and set RigidBody2D to Kinematic
         _startPosition = _rigidbody2D.position;
         _rigidbody2D.isKinematic = true;
+
         StartCoroutine(StartMenuDrag());
     }
 
+    // Starting Co-routine to handle bird launching
     IEnumerator StartMenuDrag()
     {
         yield return new WaitForSeconds(1);
-
         _animator.SetBool("_isGrabbed", true);
+
         AIDrag();
         StartCoroutine(MoveToLaunchPostition());
 
         yield return new WaitForSeconds(2);
         LetGo();
+
+        yield return new WaitForSeconds(1);
+        StartCoroutine(ResetAfterDelay());
     }
 
+    // Creates a random position for the bird to eventually move towards.
     void AIDrag()
     {
         _randomDragPosition = new Vector2(-8f, Random.Range(_minYHeight, _maxYHeight));
 
-        _desiredPosition = _randomDragPosition;
 
-        _direction = _desiredPosition - _startPosition;
+        _direction = _randomDragPosition - _startPosition;
         _direction.Normalize();
 
-        float distance = Vector2.Distance(_desiredPosition, _startPosition);
+        float distance = Vector2.Distance(_randomDragPosition, _startPosition);
 
         if (distance > _maxDragDistance)
         {
-            _desiredPosition = _startPosition + (_direction * _maxDragDistance);
+            _randomDragPosition = _startPosition + (_direction * _maxDragDistance);
         }
 
-        if (_desiredPosition.x > _startPosition.x)
-            _desiredPosition.x = _startPosition.x;
+        if (_randomDragPosition.x > _startPosition.x)
+            _randomDragPosition.x = _startPosition.x;
     }
 
+    // Turns bool on, then back off to allow movement in FixedUpdate
     IEnumerator MoveToLaunchPostition()
     {
         _canDrag = true;
         yield return new WaitForSeconds(2);
         _canDrag = false;
-
     }
 
+    // Moving Rigidbody2D position towards the random position
     void FixedUpdate()
     {
         if (_canDrag)
         {
-            _rigidbody2D.position = Vector2.MoveTowards(_rigidbody2D.position, _desiredPosition, _dragSpeed * Time.deltaTime);
+            _rigidbody2D.position = Vector2.MoveTowards(_rigidbody2D.position, _randomDragPosition, _dragSpeed * Time.deltaTime);
         }
     }
 
+    // Launches Bird by finding the direction, then adding a force
     void LetGo()
     {
         Vector2 currentPosition = _rigidbody2D.position;
@@ -97,10 +102,6 @@ public class MenuBird : MonoBehaviour
         _animator.SetBool("_isGrabbed", false);
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        StartCoroutine(ResetAfterDelay());
-    }
     IEnumerator ResetAfterDelay()
     {
         yield return new WaitForSeconds(3);
